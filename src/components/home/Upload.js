33707df985175533d6d83afb8d8a24dd034b43cb
zc <13522612182@163.com>
2016-08-03 14:39:37 +0800

    首页 临时提交

--- src/components/home/Upload.js
+++ src/components/home/Upload.js
@@ -1,21 +1,26 @@
 /**
  * Created by zc on 2016/7/26.
  */
+import manualUpload from './img/manualUpload.png';
+import imgUpload from './img/imgUpload.png';
 
 const UploadStyle = {
   right: {
     width: '20%',
     height: '240px',
     display: 'flex',
-    flexDirection: 'column',
-    alignItems: 'center',
-    textAlign: 'center',
-    lineHeight: '120px'
+    flexDirection: 'column'
   },
   rightUpload: {
     flex: 1,
-    border: '1px solid green',
-    width: '100%'
+    width: '100%',
+    display: 'flex',
+    alignItems: 'center',
+    justifyContent: 'center'
+
+  },
+  icon: {
+    width: '42px'
   }
 };
 
@@ -23,8 +28,12 @@ class Upload extends React.Component {
   render() {
     return (
       <div style={UploadStyle.right}>
-        <div style={UploadStyle.rightUpload}>手动上传</div>
-        <div style={UploadStyle.rightUpload}>拍照上传</div>
+        <div style={UploadStyle.rightUpload}>
+          <img src={manualUpload} alt="" style={UploadStyle.icon} />
+        </div>
+        <div style={UploadStyle.rightUpload}>
+          <img src={imgUpload} alt="" style={UploadStyle.icon} />
+        </div>
       </div>
     );
   }
--- webpack/webpack.config.js
+++ webpack/webpack.config.js
@@ -42,7 +42,7 @@ module.exports = {
       {test: /\.ttf(\?v=\d+\.\d+\.\d+)?$/, loader: 'url?limit=10000&mimetype=application/octet-stream'},
       {test: /\.eot(\?v=\d+\.\d+\.\d+)?$/, loader: 'file'},
       {test: /\.svg(\?v=\d+\.\d+\.\d+)?$/, loader: 'url?limit=10000&mimetype=image/svg+xml'},
-      {test: /\.jpg$/, loader: 'url'}
+      {test: /\.(png|jpg)$/, loader: 'url'}
     ]
   }
 };
