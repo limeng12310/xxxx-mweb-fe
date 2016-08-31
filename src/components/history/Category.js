a0da4f0fe05dce359588a2d336d59b6fea2fa3d9
zc <13522612182@163.com>
2016-08-31 09:50:52 +0800

    History 页面

--- src/components/history/Category.js
+++ src/components/history/Category.js
@@ -2,14 +2,14 @@
  * Created by zc on 2016/8/30.
  */
   const CategoryStyle = {
-    box:{
+    box: {
       overflowX: 'scroll',
       overflowY: 'hidden',
       width: '100%',
       height: '1rem',
       lineHeight: '1rem'
     },
-    item:{
+    item: {
       marginRight: '0.5rem',
       marginLeft: '0.5rem',
       float: 'left',
@@ -25,61 +25,61 @@
       textDecoration: 'none',
       color: '#FFF'
     }
-  }
+  };
 
   class Category extends React.Component {
-    componentDidMount(){
-        let width = 0;
-        $(this.refs.container).children('a').each(
-          function() {
-            width += (this.offsetWidth + 100);
-          });
-        width = lib.flexible.px2rem(width);
-        $(this.refs.container).css({'width':`${width}rem`});
+    componentDidMount() {
+      let width = 0;
+      $(this.refs.container).children('a').each(
+        function () {
+          width += (this.offsetWidth + 100);
+        });
+      width = lib.flexible.px2rem(width);
+      $(this.refs.container).css('width', `${width}rem`);
 
       let width2 = 0;
       $(this.refs.container2).children('a').each(
-        function() {
+        function () {
           width2 += (this.offsetWidth + 120);
         });
       width2 = lib.flexible.px2rem(width2);
-      $(this.refs.container2).css({'width':`${width2}rem`});
+      $(this.refs.container2).css('width', `${width2}rem`);
     }
     render() {
-    return (
-      <div>
-        <div className="weightLine"></div>
-        <div style={CategoryStyle.box}>
-          <div ref='container'>
-            <a href='###' style={CategoryStyle.item}>项目1</a>
-            <a href='###' style={CategoryStyle.item}>项目2</a>
-            <a href='###' style={CategoryStyle.item}>项目3</a>
-            <a href='###' style={CategoryStyle.item}>项目4</a>
-            <a href='###' style={CategoryStyle.item}>项目5</a>
-            <a href='###' style={CategoryStyle.item}>项目6</a>
-            <a href='###' style={CategoryStyle.item}>项目7</a>
-            <a href='###' style={CategoryStyle.item}>项目8</a>
-            <a href='###' style={CategoryStyle.item}>项目9</a>
-            <a href='###' style={CategoryStyle.item}>项目10</a>
+      return (
+        <div>
+          <div className="weightLine"></div>
+          <div style={CategoryStyle.box}>
+            <div ref="container">
+              <a href="###" style={CategoryStyle.item}>项目1</a>
+              <a href="###" style={CategoryStyle.item}>项目2</a>
+              <a href="###" style={CategoryStyle.item}>项目3</a>
+              <a href="###" style={CategoryStyle.item}>项目4</a>
+              <a href="###" style={CategoryStyle.item}>项目5</a>
+              <a href="###" style={CategoryStyle.item}>项目6</a>
+              <a href="###" style={CategoryStyle.item}>项目7</a>
+              <a href="###" style={CategoryStyle.item}>项目8</a>
+              <a href="###" style={CategoryStyle.item}>项目9</a>
+              <a href="###" style={CategoryStyle.item}>项目10</a>
+            </div>
           </div>
-        </div>
-        <div style={CategoryStyle.box}>
-          <div ref='container2'>
-            <a href='###' style={CategoryStyle.littleItem}>项目1</a>
-            <a href='###' style={CategoryStyle.littleItem}>项目2</a>
-            <a href='###' style={CategoryStyle.littleItem}>项目3</a>
-            <a href='###' style={CategoryStyle.littleItem}>项目4</a>
-            <a href='###' style={CategoryStyle.littleItem}>项目5</a>
-            <a href='###' style={CategoryStyle.littleItem}>项目6</a>
-            <a href='###' style={CategoryStyle.littleItem}>项目7</a>
-            <a href='###' style={CategoryStyle.littleItem}>项目8</a>
-            <a href='###' style={CategoryStyle.littleItem}>项目9</a>
-            <a href='###' style={CategoryStyle.littleItem}>项目10</a>
+          <div style={CategoryStyle.box}>
+            <div ref="container2">
+              <a href="###" style={CategoryStyle.littleItem}>项目1</a>
+              <a href="###" style={CategoryStyle.littleItem}>项目2</a>
+              <a href="###" style={CategoryStyle.littleItem}>项目3</a>
+              <a href="###" style={CategoryStyle.littleItem}>项目4</a>
+              <a href="###" style={CategoryStyle.littleItem}>项目5</a>
+              <a href="###" style={CategoryStyle.littleItem}>项目6</a>
+              <a href="###" style={CategoryStyle.littleItem}>项目7</a>
+              <a href="###" style={CategoryStyle.littleItem}>项目8</a>
+              <a href="###" style={CategoryStyle.littleItem}>项目9</a>
+              <a href="###" style={CategoryStyle.littleItem}>项目10</a>
+            </div>
           </div>
+          <div className="weightLine"></div>
         </div>
-        <div className="weightLine"></div>
-      </div>
-    );
+      );
+    }
   }
-}
-export default Category;
+  export default Category;
