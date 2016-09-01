15734b8f9ac413a02679f8b8c93c765a5209e725
limeng <upclimeng@163.com>
2016-09-01 14:39:48 +0800

    report detail

--- src/components/history/Category.js
+++ src/components/history/Category.js
@@ -15,7 +15,13 @@
       float: 'left',
       fontSize: '0.8rem',
       textDecoration: 'none',
-      color: '#FFF'
+      color: '#FFF',
+      // IE滤镜，透明度50%
+      filter: 'alpha(opacity=60)',
+      // Firefox私有，透明度50%
+      MozOpacity: 0.6,
+      // 其他，透明度50%
+      opacity: 0.6
     },
     littleItem: {
       marginRight: '0.8rem',
@@ -23,11 +29,26 @@
       float: 'left',
       fontSize: '0.6rem',
       textDecoration: 'none',
-      color: '#FFF'
+      color: '#FFF',
+      // IE滤镜，透明度50%
+      filter: 'alpha(opacity=60)',
+      // Firefox私有，透明度50%
+      MozOpacity: 0.6,
+      // 其他，透明度50%
+      opacity: 0.6
     }
   };
 
   class Category extends React.Component {
+    constructor(props) {
+      super(props);
+      this.state = {
+        isChoosenOne: 0,
+        isChoosenTwo: 0
+      };
+      this.clickChangeOne = this.clickChangeOne.bind(this);
+      this.clickChangeTwo = this.clickChangeTwo.bind(this);
+    }
     componentDidMount() {
       let width = 0;
       $(this.refs.container).children('a').each(
@@ -36,7 +57,6 @@
         });
       width = lib.flexible.px2rem(width);
       $(this.refs.container).css('width', `${width}rem`);
-
       let width2 = 0;
       $(this.refs.container2).children('a').each(
         function () {
@@ -45,36 +65,90 @@
       width2 = lib.flexible.px2rem(width2);
       $(this.refs.container2).css('width', `${width2}rem`);
     }
+    clickChangeOne(e) {
+      this.setState({
+        isChoosenOne: parseInt(e.target.getAttribute('data-index'), 10),
+        isChoosenTwo: 0
+      });
+      this.props.handleChangeDataOne(parseInt(e.target.getAttribute('data-index'), 10));
+    }
+    clickChangeTwo(e) {
+      this.setState({
+        isChoosenTwo: parseInt(e.target.getAttribute('data-index'), 10)
+      });
+      this.props.handleChangeDataTwo(
+        parseInt(e.target.getAttribute('data-index'), 10),
+        e.target.getAttribute('data-name')
+      );
+    }
     render() {
+      // let containerOne;
+      // let containerTwo;
+      // if(this.props.itemListOne)
       return (
         <div>
           <div className="weightLine"></div>
           <div style={CategoryStyle.box}>
             <div ref="container">
-              <a href="###" style={CategoryStyle.item}>项目1</a>
-              <a href="###" style={CategoryStyle.item}>项目2</a>
-              <a href="###" style={CategoryStyle.item}>项目3</a>
-              <a href="###" style={CategoryStyle.item}>项目4</a>
-              <a href="###" style={CategoryStyle.item}>项目5</a>
-              <a href="###" style={CategoryStyle.item}>项目6</a>
-              <a href="###" style={CategoryStyle.item}>项目7</a>
-              <a href="###" style={CategoryStyle.item}>项目8</a>
-              <a href="###" style={CategoryStyle.item}>项目9</a>
-              <a href="###" style={CategoryStyle.item}>项目10</a>
+              {
+                this.props.itemListOne.map((item, i) => {
+                  let colorChoosen = {};
+                  if (this.state.isChoosenOne === i) {
+                    colorChoosen = {
+                      // IE滤镜，透明度50%
+                      filter: 'alpha(opacity=100)',
+                      // Firefox私有，透明度50%
+                      MozOpacity: 1,
+                      // 其他，透明度50%
+                      opacity: 1
+                    };
+                  }
+                  return (
+                    <a
+                      key={i}
+                      data-index={i}
+                      style={Object.assign({}, CategoryStyle.item, colorChoosen)}
+                      onClick={this.clickChangeOne}
+                    >
+                      {item.name}
+                    </a>
+                  );
+                })
+              }
             </div>
           </div>
           <div style={CategoryStyle.box}>
             <div ref="container2">
-              <a href="###" style={CategoryStyle.littleItem}>项目1</a>
-              <a href="###" style={CategoryStyle.littleItem}>项目2</a>
-              <a href="###" style={CategoryStyle.littleItem}>项目3</a>
-              <a href="###" style={CategoryStyle.littleItem}>项目4</a>
-              <a href="###" style={CategoryStyle.littleItem}>项目5</a>
-              <a href="###" style={CategoryStyle.littleItem}>项目6</a>
-              <a href="###" style={CategoryStyle.littleItem}>项目7</a>
-              <a href="###" style={CategoryStyle.littleItem}>项目8</a>
-              <a href="###" style={CategoryStyle.littleItem}>项目9</a>
-              <a href="###" style={CategoryStyle.littleItem}>项目10</a>
+              {
+                this.props.itemListTwo.map((item, i) => {
+                  let circleChoosen = {};
+                  if (this.state.isChoosenTwo === i) {
+                    circleChoosen = {
+                      // IE滤镜，透明度50%
+                      filter: 'alpha(opacity=100)',
+                      // Firefox私有，透明度50%
+                      MozOpacity: 1,
+                      // 其他，透明度50%
+                      opacity: 1,
+                      borderWidth: '0.026667rem',
+                      borderStyle: 'solid',
+                      borderRadius: '0.266667rem',
+                      display: 'table'
+                    };
+                  }
+                  return (
+                    <a
+                      data-unit={item.unit}
+                      key={i}
+                      data-index={i}
+                      style={Object.assign({}, CategoryStyle.littleItem, circleChoosen)}
+                      onClick={this.clickChangeTwo}
+                    >
+                      {item.name}
+                    </a>
+                  );
+                })
+              }
             </div>
           </div>
           <div className="weightLine"></div>
@@ -82,4 +156,10 @@
       );
     }
   }
+  Category.propTypes = {
+    itemListOne: React.PropTypes.array,
+    itemListTwo: React.PropTypes.array,
+    handleChangeDataOne: React.PropTypes.func,
+    handleChangeDataTwo: React.PropTypes.func
+  };
   export default Category;
