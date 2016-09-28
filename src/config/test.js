3584bc68af8194b957cddf59df96da98993c134d
lczhou <zlc@lchzou.me>
2016-09-28 11:39:39 +0800

    test config oss

--- src/config/test.js
+++ src/config/test.js
@@ -1,4 +1,11 @@
 module.exports = {
   apiPrefix: 'http://test.thorgene.com/thorgene-mweb-api',
-  cdnPrefix: 'http://thorgene-mweb.img-cn-beijing.aliyuncs.com'
+  cdnPrefix: 'http://thorgene-mweb.img-cn-beijing.aliyuncs.com',
+  aliOss: {
+    region: 'oss-cn-beijing',
+    accessKeyId: 'LTAI5xNS9BSFbw9D',
+    accessKeySecret: 'AHcLoGcxRtq62U1R24skgIqo9igTxg',
+    bucket: 'thorgene-mweb',
+    ossPrefix: 'http://thorgene-mweb.oss-cn-beijing.aliyuncs.com'
+  }
 };
