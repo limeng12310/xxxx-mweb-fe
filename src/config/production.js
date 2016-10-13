0d77dfb3881331f20416aa225895b326692acd3c
lczhou <zlc@lchzou.me>
2016-10-13 17:00:10 +0800

    v2.0.1上线 bugfix

---
+++ src/config/production.js
@@ -0,0 +1,11 @@
+module.exports = {
+  apiPrefix: 'http://thorgene.com/thorgene-mweb-api',
+  cdnPrefix: 'http://thorgene-mweb-prod.img-cn-beijing.aliyuncs.com',
+  aliOss: {
+    region: 'oss-cn-beijing',
+    accessKeyId: 'LTAI5xNS9BSFbw9D',
+    accessKeySecret: 'AHcLoGcxRtq62U1R24skgIqo9igTxg',
+    bucket: 'thorgene-mweb-prod',
+    ossPrefix: 'http://thorgene-mweb-prod.oss-cn-beijing.aliyuncs.com'
+  }
+};
---
+++ webpack/webpack.prod.config.js
@@ -0,0 +1,78 @@
+/* eslint-disable */
+var path = require('path');
+var webpack = require('webpack');
+var HtmlWebpackPlugin = require('html-webpack-plugin');
+var CopyWebpackPlugin = require('copy-webpack-plugin');
+
+module.exports = {
+  entry: {
+    home: [
+      'whatwg-fetch',
+      'zepto/src/zepto',
+      'zepto/src/event',
+      'zepto/src/touch',
+      'zepto/src/fx',
+      './src/index.js'
+    ],
+    index: [
+      'whatwg-fetch',
+      './src/entry.js'
+    ],
+    profile: [
+      'whatwg-fetch',
+      './src/profile.js'
+    ]
+  },
+  debug: false,
+  output: {
+    path: path.join(__dirname, '../dist'),
+    filename: '[name].[hash:8].js',
+    publicPath: ''
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: 'Thorgene',
+      template: 'src/index.ejs',
+      filename: 'app-home.html',
+      chunks: ['home']
+    }),
+    new HtmlWebpackPlugin({
+      title: 'Thorgene',
+      filename: 'index.html',
+      chunks: ['index']
+    }),
+    new HtmlWebpackPlugin({
+      title: 'Thorgene',
+      filename: 'init-profile.html',
+      chunks: ['profile']
+    }),
+    new CopyWebpackPlugin([
+      { from: './assets/' }
+    ]),
+    new webpack.ProvidePlugin({
+      React: 'react'
+    }),
+    new webpack.DefinePlugin({
+      ENV: JSON.stringify('production'),
+      CORDOVA_ENV: process.env.CORDOVA_ENV ? JSON.stringify('true') : JSON.stringify('false')
+    })
+  ],
+  module: {
+    loaders: [
+      {
+        test: /\.js$/,
+        loaders: ['babel'],
+        exclude: /node_modules/
+      },
+      {
+        test: /\.css?$/,
+        loaders: ['style', 'css']
+      },
+      {test: /\.(woff|woff2)(\?v=\d+\.\d+\.\d+)?$/, loader: 'url?limit=10000&mimetype=application/font-woff'},
+      {test: /\.ttf(\?v=\d+\.\d+\.\d+)?$/, loader: 'url?limit=10000&mimetype=application/octet-stream'},
+      {test: /\.eot(\?v=\d+\.\d+\.\d+)?$/, loader: 'file'},
+      {test: /\.svg(\?v=\d+\.\d+\.\d+)?$/, loader: 'url?limit=10000&mimetype=image/svg+xml'},
+      {test: /\.(jpg|png)$/, loader: 'url'}
+    ]
+  }
+};
