84b9dd30bce9fec1f3911bcb368a6b93e06fc23c
zhaoyuxiang <112408584@qq.com>
2016-10-13 14:45:21 +0800

    toast/processIndicator组件开发完成

--- src/components/common/ProcessIndicator.js
+++ src/components/common/ProcessIndicator.js
@@ -1,74 +1,71 @@
 import loadingIcon from './img/loadingIcon.png';
 
 class ProcessIndicator extends React.Component {
-	constructor(props) {
-		super(props);
-	}
 
-	componentWillMount() {
-		this.setState({
-			open: this.props.open,
-			message: this.props.message,
-		});
-	}
+  componentWillMount() {
+    this.setState({
+      open: this.props.open,
+      message: this.props.message
+    });
+  }
 
-	componentWillReceiveProps(nextProps) {
-		const open = nextProps.open;
+  componentWillReceiveProps(nextProps) {
+    const open = nextProps.open;
 
-		this.setState({
-			open: open !== null ? open : this.state.open,
-			message: nextProps.message
-		});
-	}
+    this.setState({
+      open: open !== null ? open : this.state.open,
+      message: nextProps.message
+    });
+  }
 
-	render() {
-		const {
-			message
-		}=this.props;
-		const styles = {
-			background: {
-				zIndex: '9999',
-				position: 'fixed',
-				width: '100%',
-				height: '100%',
-				backgroundColor: 'rgba(0,0,0,0.5)',
-				textAlign: 'center',
-				display: this.state.open ? 'flex' : 'none',
-				justifyContent: 'middle'
-			},
-			processIndicatorContent: {
-				display: 'inline-block',
-				margin: 'auto',
-				minWidth: '6.64rem',
-				minHeight: '2.15rem',
-				color: '#000',
-				backgroundColor: '#fff',
-				textAlign: 'center',
-				borderRadius: '0.28rem',
-				padding: '0 0.5rem'
-			},
-			loadingIcon: {
-				width: '0.8rem',
-				height: '0.8rem',
-				display:'block',
-				margin:'0.2rem auto',
-				animation:'loading 1.5s linear infinite'
-			}
-		};
-		return (
-			<div style={styles.background}>
-				<div style={styles.processIndicatorContent}>
-					<img src={loadingIcon} style={styles.loadingIcon} alt=""/>
-					{message}
-				</div>
-			</div>
-		);
-	}
+  render() {
+    const {
+      message
+    } = this.props;
+    const styles = {
+      background: {
+        zIndex: '9999',
+        position: 'fixed',
+        width: '100%',
+        height: '100%',
+        backgroundColor: 'rgba(0,0,0,0.5)',
+        textAlign: 'center',
+        display: this.state.open ? 'flex' : 'none',
+        justifyContent: 'middle'
+      },
+      processIndicatorContent: {
+        display: 'inline-block',
+        margin: 'auto',
+        minWidth: '6.64rem',
+        minHeight: '2.15rem',
+        color: '#000',
+        backgroundColor: '#fff',
+        textAlign: 'center',
+        borderRadius: '0.28rem',
+        padding: '0 0.5rem'
+      },
+      loadingIcon: {
+        width: '0.8rem',
+        height: '0.8rem',
+        display: 'block',
+        margin: '0.2rem auto',
+        animation: 'loading 1.5s linear infinite'
+      }
+    };
+    return (
+      <div style={styles.background}>
+        <div style={styles.processIndicatorContent}>
+          <img src={loadingIcon} style={styles.loadingIcon} alt="" />
+          {message}
+        </div>
+      </div>
+    );
+  }
 }
 
 ProcessIndicator.propTypes = {
-	message: React.PropTypes.string,
-	open: React.PropTypes.bool
+  message: React.PropTypes.string,
+  open: React.PropTypes.bool
 };
 
 export default ProcessIndicator;
--- src/components/common/Toast.js
+++ src/components/common/Toast.js
@@ -1,144 +1,125 @@
 class Toast extends React.Component {
-	constructor(props) {
-		super(props);
-	}
 
-	componentWillMount() {
-		this.setState({
-			open: this.props.open,
-			message: this.props.message,
-			duration: this.props.duration,
-		});
-	}
+  componentWillMount() {
+    this.setState({
+      open: this.props.open,
+      message: this.props.message,
+      duration: this.props.duration
+    });
+  }
 
-	componentDidMount() {
-		if (this.state.open) {
-			this.setAutoHideTimer();
-			//this.setTransitionTimer();
-		}
-	}
+  componentDidMount() {
+    if (this.state.open) {
+      this.setAutoHideTimer();
+    }
+  }
 
-	componentWillReceiveProps(nextProps) {
-		if (this.props.open && nextProps.open &&
-			(nextProps.message !== this.props.message)) {
-			this.setState({
-				open: false,
-			});
+  componentWillReceiveProps(nextProps) {
+    if (this.props.open && nextProps.open &&
+      (nextProps.message !== this.props.message)) {
+      this.setState({
+        open: false
+      });
 
-			clearTimeout(this.timerOneAtTheTimeId);
-			this.timerOneAtTheTimeId = setTimeout(() => {
-				this.setState({
-					message: nextProps.message,
-					open: true,
-				});
-			}, 400);
-		} else {
-			const open = nextProps.open;
+      clearTimeout(this.timerOneAtTheTimeId);
+      this.timerOneAtTheTimeId = setTimeout(() => {
+        this.setState({
+          message: nextProps.message,
+          open: true
+        });
+      }, 400);
+    } else {
+      const open = nextProps.open;
 
-			this.setState({
-				open: open !== null ? open : this.state.open,
-				message: nextProps.message
-			});
-		}
-	}
+      this.setState({
+        open: open !== null ? open : this.state.open,
+        message: nextProps.message
+      });
+    }
+  }
 
-	componentDidUpdate(prevProps, prevState) {
-		if (prevState.open !== this.state.open) {
-			if (this.state.open) {
-				this.setAutoHideTimer();
-				//this.setTransitionTimer();
-			} else {
-				clearTimeout(this.timerAutoHideId);
-			}
-		}
-	}
+  componentDidUpdate(prevProps, prevState) {
+    if (prevState.open !== this.state.open) {
+      if (this.state.open) {
+        this.setAutoHideTimer();
+      } else {
+        clearTimeout(this.timerAutoHideId);
+      }
+    }
+  }
 
-	componentWillUnmount() {
-		clearTimeout(this.timerAutoHideId);
-		//clearTimeout(this.timerTransitionId);
-		clearTimeout(this.timerOneAtTheTimeId);
-	}
+  componentWillUnmount() {
+    clearTimeout(this.timerAutoHideId);
+    clearTimeout(this.timerOneAtTheTimeId);
+  }
 
-	// componentClickAway = () => {
-	// 	if (this.timerTransitionId) {
-	// 		// If transitioning, don't close the snackbar.
-	// 		return;
-	// 	}
-	//
-	// 	if (this.props.open !== null && this.props.onRequestClose) {
-	// 		this.props.onRequestClose('clickaway');
-	// 	} else {
-	// 		this.setState({open: false});
-	// 	}
-	// };
+  // Timer that controls delay before snackbar auto hides
+  setAutoHideTimer() {
+    const duration = this.props.duration;
 
-	// Timer that controls delay before snackbar auto hides
-	setAutoHideTimer() {
-		const duration = this.props.duration;
+    if (duration > 0) {
+      clearTimeout(this.timerAutoHideId);
+      this.timerAutoHideId = setTimeout(() => {
+        if (this.props.open !== null && this.props.closeHandler) {
+          this.props.closeHandler('timeout');
+        } else {
+          this.setState({open: false});
+        }
+      }, duration);
+    }
+  }
 
-		if (duration > 0) {
-			clearTimeout(this.timerAutoHideId);
-			this.timerAutoHideId = setTimeout(() => {
-				if (this.props.open !== null && this.props.closeHandler) {
-					this.props.closeHandler('timeout');
-				} else {
-					this.setState({open: false});
-				}
-			}, duration);
-		}
-	}
+  // Timer that controls delay before click-away events are captured (based on when animation completes)
+  // setTransitionTimer() {
+  // 	this.timerTransitionId = setTimeout(() => {
+  // 		this.timerTransitionId = undefined;
+  // 	}, 400);
+  // }
 
-	// Timer that controls delay before click-away events are captured (based on when animation completes)
-	// setTransitionTimer() {
-	// 	this.timerTransitionId = setTimeout(() => {
-	// 		this.timerTransitionId = undefined;
-	// 	}, 400);
-	// }
-
-	render() {
-		console.log(this.state.open);
-		const {
-			message
-		}=this.props;
-		const duration = this.props.duration - 400;
-		const styles = {
-			background: {
-				zIndex: '9999',
-				top: '50%',
-				position: 'fixed',
-				width: '100%',
-				textAlign: 'center',
-				display: this.state.open ? 'block' : 'none',
-				animation: `fadeOut .3s linear ${duration}ms`,
-				animationFillMode: 'forwards'
-			},
-			alertContent: {
-				display: 'inline-block',
-				margin: '0 auto',
-				minWidth: '3.1rem',
-				minHeight: '0.56rem',
-				color: '#fff',
-				backgroundColor: 'rgba(0,0,0,0.2)',
-				textAlign: 'center',
-				borderRadius: '0.28rem',
-				padding: '0 0.5rem'
-			}
-		};
-		return (
-			<div style={styles.background}>
-				<div style={styles.alertContent}>
-					{message}
-				</div>
-			</div>
-		);
-	}
+  render() {
+    console.log(this.state.open);
+    const {
+      message
+    } = this.props;
+    const duration = this.props.duration - 400;
+    const styles = {
+      background: {
+        zIndex: '9999',
+        top: '50%',
+        position: 'fixed',
+        width: '100%',
+        textAlign: 'center',
+        display: this.state.open ? 'block' : 'none',
+        animation: `fadeOut .3s linear ${duration}ms`,
+        animationFillMode: 'forwards'
+      },
+      alertContent: {
+        display: 'inline-block',
+        margin: '0 auto',
+        minWidth: '3.1rem',
+        minHeight: '0.56rem',
+        color: '#fff',
+        backgroundColor: 'rgba(0,0,0,0.2)',
+        textAlign: 'center',
+        borderRadius: '0.28rem',
+        padding: '0 0.5rem'
+      }
+    };
+    return (
+      <div style={styles.background}>
+        <div style={styles.alertContent}>
+          {message}
+        </div>
+      </div>
+    );
+  }
 }
 
 Toast.propTypes = {
-	message: React.PropTypes.string,
-	open: React.PropTypes.bool,
-	duration: React.PropTypes.number,
-	closeHandler: React.PropTypes.func
+  message: React.PropTypes.string,
+  open: React.PropTypes.bool,
+  duration: React.PropTypes.number,
+  closeHandler: React.PropTypes.func
 };
 
 export default Toast;
